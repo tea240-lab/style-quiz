9d9f4e4c93f5383360a2e65f84b90f495d0deb4c
tea240-lab <tea240@sgihs.tc.edu.tw>
2026-09-16 14:59:12 +0800

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration settings for the project.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 設定
+const firebaseConfig = {
+  apiKey: "AIzaSyByP8AR7RecUcPLJZdB3wKVKYMFHzXs0Xs",
+  authDomain: "style-monopoly.firebaseapp.com",
+  databaseURL: "https://style-monopoly-default-rtdb.firebaseio.com",
+  projectId: "style-monopoly",
+  storageBucket: "style-monopoly.firebasestorage.app",
+  messagingSenderId: "857512755838",
+  appId: "1:857512755838:web:c32415b959e1ad0cdc8e2f"
+};
